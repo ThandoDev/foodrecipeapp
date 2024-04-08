fa74afec14ad993328e8d20a16f08fb6e427cf70
ThandokuhleNdlebe <thandotydx@gmail.com>
2024-04-08 23:33:14 +0200

    added images and new changes to hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,23 @@
+import CustomImage from "./CustomImage" 
+
 export default function HeroSection(){
     return (
-        <div className="section">
+        <div className="section hero">
             <div className="col">
             <h1 className="title">What Are We About</h1>
             <p className="info">FlavourBoys is a place where you can please your soul and tummy with delicious food recipes of all cuisine and our service is absolutely free. So start exploring now</p>
             <button className="btn">explore now</button>
             </div>
-            <div className="col">
-            col 2
+            <div className="col gallery">
+            <CustomImage pt={"70%"}/> 
+            <CustomImage pt={"70%"}/> 
+            <CustomImage pt={"70%"}/> 
+            <CustomImage pt={"70%"}/> 
+            <CustomImage pt={"70%"}/> 
+            <CustomImage pt={"70%"}/> 
+            <CustomImage pt={"70%"}/> 
+            <CustomImage pt={"70%"}/> 
+            <CustomImage pt={"70%"}/> 
             </div>
              </div>
     )
