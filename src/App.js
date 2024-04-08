3621cac86122d7c000444cad08a073453d9270a1
ThandokuhleNdlebe <thandotydx@gmail.com>
2024-04-08 19:04:44 +0200

    few changes to the navbar title

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Navbar from "./components/navbar"
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
   <Navbar />
-=======
-
->>>>>>> 39e54f2eea24f71055454c6b6b59bf2e5f24ac88
     </div>
   );
 }
